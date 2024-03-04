6ec7da3dac6c0479fc959332d3967304f80d5832
maleyk01 <159915264+maleyk01@users.noreply.github.com>
2024-03-04 15:12:52 +0300

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -1,4 +1,4 @@
 name = input("what is your name? ")
-age  = input(int("How old are you? "))
+age  = int(input("How old are you? "))
 location = input("where do you live? ")
-print (f"my name is {name}. \n I'm {age} years old. \n I live at {location}")
+print (f" My name is {name}. \n I'm {age} years old. \n I live at {location}")
